94cb8966a78b6b2020b752271338a592956e775e
A-M-I <160208713+Alisha000111@users.noreply.github.com>
2024-05-12 15:14:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// let increment = 1;
+//increment++
+// console.log(++increment)
+// for(let a = 1; a <= 1000; a++){
+// console.log("Thank You Governor Kamran Khan Tessori Sahab");
+// console.log("Pakistan Zindabad");
+// console.log(a);
+// console.log("My Name is Alisha");
+// console.log("My Name is Tania");
+// console.log("My Name is Ifra");
+// }
+//  const cars = ["Rolls Royce", "Bently", "F1","R35 Skyline"]
+//  for(let i = 0; i <= 4; i++){
+//  console.log("My favourite car is +cars[i]");
+//  }
+var numbers = [20, 300, 6000, 90000];
+for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
+    var n = numbers_1[_i];
+    console.log(n);
+    break;
+}
+var num = [1111, 2222, 3333, 4444, 5555];
+for (var i in num) {
+    console.log(i + "=" + num[i]);
+}
+var p = 5;
+while (p <= 25) {
+    console.log(p);
+    p++;
+}
+var d = 3;
+do {
+    console.log(d);
+    d++;
+} while (d >= 30);
---
+++ app.ts
@@ -0,0 +1,35 @@
+// let increment = 1;
+//increment++
+// console.log(++increment)
+// for(let a = 1; a <= 1000; a++){
+    // console.log("Thank You Governor Kamran Khan Tessori Sahab");
+    // console.log("Pakistan Zindabad");
+    // console.log(a);
+    // console.log("My Name is Alisha");
+    // console.log("My Name is Tania");
+    // console.log("My Name is Ifra");
+ // }
+//  const cars = ["Rolls Royce", "Bently", "F1","R35 Skyline"]
+//  for(let i = 0; i <= 4; i++){
+//  console.log("My favourite car is +cars[i]");
+//  }
+let numbers = [20,300,6000,90000];
+for(let n of numbers){
+    console.log(n);
+    break;
+}
+let num = [1111,2222,3333,4444,5555];
+for(let i in num){
+    console.log(i + "=" + num[i]);
+}
+let p = 5;
+while(p<=25){
+console.log(p);
+p++;
+}
+let d = 3;
+do{
+    console.log(d);
+    d++;
+}
+while(d>=30);
